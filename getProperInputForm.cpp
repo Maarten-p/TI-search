d5a57915188571bd00eee65a493536920d6f852c
mpieck <mpieck@loemelaer.esat.kuleuven.be>
2017-08-07 11:55:11 +0200

    started work on properInputForm

--- getProperInputForm.cpp
+++ getProperInputForm.cpp
@@ -1,25 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   newmain.cpp
- * Author: mpieck
- *
- * Created on August 7, 2017, 11:06 AM
- */
-
 #include <cstdlib>
 
-using namespace std;
+bool getANF(const int f[N], int p[N]){
+    int i, j;
+    for (i = 0; i < N; ++i) p[i] = f[i];
 
-/*
- * 
- */
+    for (i = 1; i < N; i <<= 1){
+        for (j = i; j < N; j = (j + 1) | i){
+            p[j] ^= p[j ^ i];
+        }
+    }
+    return true;
+}
+
+bool ANFToProperInput(const int p[N], const std::string& filename) {
+    
+}
+
 int main(int argc, char** argv) {
-
+    int N = argv[0]
+    int p[N] = {};
+    getANF(argv[1],p);
     return 0;
 }
 
--- main.cpp
+++ main.cpp
@@ -767,17 +767,7 @@ std::vector<std::vector<BlnFunction>> readRealization(const std::string& filenam
     return result;
 }
 
-//bool getANF(const int f[N], int p[N]){
-//    int i, j;
-//    for (i = 0; i < N; ++i) p[i] = f[i];
-//
-//    for (i = 1; i < N; i <<= 1){
-//        for (j = i; j < N; j = (j + 1) | i){
-//            p[j] ^= p[j ^ i];
-//        }
-//    }
-//    return true;
-//}
+
 
 int main(int argc, char *argv[])
 {
